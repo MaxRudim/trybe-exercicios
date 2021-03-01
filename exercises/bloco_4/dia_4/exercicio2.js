e9ecbf7fc128cd83afce1521055874bab29dc39c
Max Rudim <max.hermsdorff@gmail.com>
2021-03-01 19:37:00 -0300

    exercicios bloco 4 dia 4

---
+++ exercises/bloco_4/dia_4/exercicio2.js
@@ -0,0 +1,155 @@
+/* Gabarito
+function verificaPalindrome(palavra) {
+    let arrayLetras = palavra.split("");
+    let isPalindrome = true;
+    for (let index in arrayLetras) {
+      if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+        isPalindrome = false;
+      }
+    }
+    return isPalindrome;
+  }
+  
+  function verificaPalindrome(string) {
+    let reverse = string.split("").reverse().join("");
+    if (reverse === string) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  console.log(verificaPalindrome('arara')); 
+  console.log(verificaPalindrome('desenvolvimento'));
+
+*/
+/* gabarito
+function indiceDoMaior (numeros) {
+    let indiceMaior = 0;
+    for (let indice in numeros) {
+        if (numeros[indiceMaior] < numeros[indice]) {
+            indiceMaior = indice;
+        }
+    }
+    return indiceMaior;
+}
+console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]));
+*/
+/* só trocar valor do < para >
+function indiceDoMenor(numeros) {
+  let indiceMenor = 0;
+  for (let indice in numeros) {
+    if (numeros[indiceMenor] > numeros[indice]) {
+      indiceMenor = indice;
+    }
+  }
+  return indiceMenor;
+}
+
+console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3])); //6
+*/
+/* gab.
+function maiorPalavra(palavras) {
+  let maiorPalavra = palavras[0];
+  for (let indice in palavras) {
+    if (maiorPalavra.length < palavras[indice].length) {
+      maiorPalavra = palavras[indice];
+    }
+  }
+  return maiorPalavra;
+}
+
+console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); //Fernanda
+*/
+
+/* gab.
+//Duas formas de resolver o exercício
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero++;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+  return numeros[indexNumeroRepetido];
+}
+
+function maisRepetido(numeros) {
+  let num = {};
+
+  for (let i = 0; i < numeros.length; i += 1) {
+    let valor = numeros[i];
+    if (num[valor] === undefined) {
+      num[valor] = 1;
+    } else {
+      num[valor] = num[valor] + 1;
+    }
+  }
+
+  let contRepetido = 0;
+  let contNumero = 0;
+
+  for (let prop in num) {
+    if (contRepetido < num[prop]) {
+      contRepetido = num[prop];
+      contNumero = prop;
+    }
+  }
+
+  return contNumero;
+}
+
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3])); //2
+*/
+
+/* gab.
+function somaTodosNumeros(numeros) {
+  let total = 0;
+  for (let index = 1; index <= numeros; index++) {
+    total = total + index;
+  }
+  return total;
+}
+console.log(somaTodosNumeros(5)); //15
+*/
+
+/* gab.
+//Duas formas de resolver o exercício
+function verificaFimPalavra(palavra, fimPalavra) {
+  palavra = palavra.split("");
+  fimPalavra = fimPalavra.split("");
+  controle = true;
+  for (let i = 0; i < fimPalavra.length; i++) {
+    if (palavra[palavra.length-fimPalavra.length+i] != fimPalavra[i]) {
+      controle = false;
+    }
+  }
+  return controle;
+}
+
+function verificaFimPalavra(palavra, fimPalavra) {
+  let inversoPalavra = palavra.split("").reverse().join("");
+  let inversoFimPalavra = fimPalavra.split("").reverse().join("");
+
+  for (let i = 0; i < inversoFimPalavra.length; i += 1) {
+    if (inversoPalavra[i] != inversoFimPalavra[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+console.log(verificaFimPalavra("trybe", "be")); //true
+console.log(verificaFimPalavra("joaofernando", "fernan")); //false
+*/
